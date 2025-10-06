92e3e4ab88575882127ce3a608510d4926abeb4c
Reseptinurkka <reseptinurkka@gmail.com>
2025-10-06 19:34:45 +0300

    Add searchfield

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import pagefind from "astro-pagefind";
 
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-   vite: {
+  vite: {
     plugins: [tailwindcss()],
   },
   markdown: {
     shikiConfig: {
-      theme: "css-variables"
-    }
+      theme: "css-variables",
+    },
   },
-  site: 'https://Reseptinurkka.github.io',
-  integrations: [sitemap(), mdx()] });
+  site: "https://Reseptinurkka.github.io",
+  integrations: [sitemap(), mdx(), pagefind()],
+});
+
